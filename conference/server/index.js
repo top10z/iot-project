06f2715223b604669ce4e8e8a49c1734c68e916d
Ankit Binjola <abinjola.binjola@gmail.com>
2018-11-28 19:36:33 +0530

    ngoMean_day_1

---
+++ conference/server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const routes = require('./routes');
+app.use(express.static('public'));
+app.get('/favicon.ico',(req, res, next) =>{
+    return res.sendStatus(204);
+});
+
+app.use('/',routes());
+
+app.listen(3000);
+
+module.export = app;
---
+++ conference/server/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+module.exports = () => {
+    router.get('/',(req, res, next) =>{
+        return res.send('Index');
+    });
+
+    router.use('/speakers', speakersRoute());
+    router.use('/feedback', feedbackRoute());
+    return router;
+}
